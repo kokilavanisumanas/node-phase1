44e3c022f45c3a68f5d06a35b544881eed013f81
kokilavani <kokialvani.p@arkinfotec.com>
2026-01-21 18:38:39 +0530

    PHASE 1:TASK 1.1 – Express App Configuration

---
+++ README.md
@@ -0,0 +1,8 @@
+#PHASE 1: Express & API Basics
+
+TASK 1.1 – Express App Configuration
+Goal:
+- JSON body parsing
+- URL-encoded data parsing
+- Centralized route registration
+- /api/v1 base route
--- src/app.js
+++ src/app.js
@@ -8,4 +8,18 @@ app.get('/health', (req, res) => {
   });
 });
 
+//parse incoming Json requests
+app.use(express.json());
+
+//parse URL-encoded data(from-submissions)
+app.use(express.urlencoded({extended: true}))
+
+//Base API routes
+app.get("/api/v1",(req, res)=>{
+  res.status(200).json({
+    success:true,
+    message:"Api is wokring fine"
+  });
+})
+
 module.exports = app;
